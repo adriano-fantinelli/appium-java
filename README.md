0acd8e7639a58ab90bc911ed8a0237ae0ed0afaf
ADRIANO FANTINELLI <adriano.fantinelli-ext@viavarejo.com.br>
2021-02-26 16:12:56 -0300

    Starting section 6.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Install java , android studio, intellij, appium, vscode
 prepare the backend
 
 npm i inside the react-native-app
+
+npx react-native run-android
+
+install chrome webdriver
--- appium_automation/src/test/java/OpenTheApp.java
+++ appium_automation/src/test/java/OpenTheApp.java
@@ -1,17 +1,68 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class OpenTheApp {
 
+//    @Test
+//    public void openAppOnAndroid() throws MalformedURLException {
+//
+//        String url = "http://127.0.0.1:4723/wd/hub";
+//
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus_5X_API_28");
+//        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+//        caps.setCapability(MobileCapabilityType.APP, "C:\\git-rep\\appium-java\\react-native-app\\android\\app\\build\\outputs\\apk\\debug\\app-debug.apk");
+////        caps.setCapability(MobileCapabilityType.ORIENTATION, "LANDSCAPE");
+//        AppiumDriver driver = new AppiumDriver(new URL(url), caps);
+//
+//    }
+
+//    @Test
+//    public void openWebAppOnAndroid() throws MalformedURLException {
+//
+//        WebDriverManager.chromedriver().browserVersion("69").setup();
+//
+//        String url = "http://127.0.0.1:4723/wd/hub";
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus_5X_API_28");
+//        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+//        caps.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+//        caps.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_EXECUTABLE, WebDriverManager.chromedriver().getBinaryPath());
+//        AppiumDriver driver = new AppiumDriver(new URL(url), caps);
+//
+//        driver.get("https://google.com");
+//    }
+
     @Test
-    public void openAppOnAndroid(){
+    public void openApreInstalledAppOnAndroid() throws MalformedURLException {
 
+        WebDriverManager.chromedriver().browserVersion("69").setup();
+
+        String url = "http://127.0.0.1:4723/wd/hub";
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 5X API 28");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus_5X_API_28");
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-        caps.setCapability(MobileCapabilityType.APP, "C:\\git-rep\\appium-java\\react-native-app\\android\\app\\build\\outputs\\apk\\debug\\app-debug.apk");
+        caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.deskclock");
+        caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.deskclock.DeskClock");
+        AppiumDriver driver = new AppiumDriver(new URL(url), caps);
 
+//        driver.findElementById("com.google.android.deskclock:id/fab").click();
+//        driver.findElementByClassName("android.widget.ImageButton").click();
+//        driver.findElementByAccessibilityId("Cities").click();
+        driver.findElementByXPath("//*[@text='ALARM']").click();
+
+//        XPATH
+//        //className[@attribute = 'value']
+//        android.widget.TextView[@text='ALARM']
+//        *
     }
 }
